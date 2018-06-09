2b4f23960625b286cf1645e36d6d66763650eaf1
Axel Kaliff <axel.kaliff@gmail.com>
2018-06-09 18:03:17 +0200

    Initial Commit

---
+++ Food Share/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Food Share
+//
+//  Created by Axel Kaliff on 2018-06-09.
+//  Copyright © 2018 Axel Kaliff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
